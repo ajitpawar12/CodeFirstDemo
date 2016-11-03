9bd3a24932b471ad819f2bb708d0e7955bc51916
ajit <ajit@idpsolutions.com.au>
2016-11-03 17:03:10 +0530

    Add project files.

---
+++ CodeFirstDemo/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CodeFirstDemo.Models;
+
+namespace CodeFirstDemo.Context
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CodeFirstDemo/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstDemo.Context;
+using CodeFirstDemo.Models;
+using System.IO;
+
+namespace CodeFirstDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductContext db=new ProductContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            Product product=new Product();
+            
+            return View(db.Products.ToList());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            Product products=new Product();
+
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product,HttpPostedFileBase image1)
+        {
+            
+            if (image1 != null && image1.ContentLength > 0)
+            {
+                var name = System.IO.Path.GetFileName(image1.FileName);
+               
+                var path = "images/" + name;
+
+                image1.SaveAs(Server.MapPath("~/images/"+name));
+                product.Picture= path;
+            }
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+            //try
+            //{
+            //    // TODO: Add insert logic here
+
+            //    return RedirectToAction("Index");
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
