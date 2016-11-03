7e87fa7d9b08a09fff90f556de0fed712fe913ff
ajit <ajit@idpsolutions.com.au>
2016-11-03 18:14:20 +0530

    first commit

---
+++ CodeFirstDemo/Migrations/201610271232568_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace CodeFirstDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Double(nullable: false),
+                        Picture = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ CodeFirstDemo/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace CodeFirstDemo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstDemo.Context.ProductContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstDemo.Context.ProductContext";
+        }
+
+        protected override void Seed(CodeFirstDemo.Context.ProductContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
